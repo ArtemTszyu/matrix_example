7d4a879f594e841c57de5390caaedb5652562cfd
ArtemTszyu <32194510+ArtemTszyu@users.noreply.github.com>
2018-03-06 00:05:29 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,20 +1,54 @@
 #include "matrix.hpp"
 
-matrix_t::matrix_t() : elements_{ nullptr }, rows_{ 0 }, collumns_{ 0 }
+matrix_t::matrix_t()
+    : elements_{ nullptr }
+    , rows_{ 0 }
+    , collumns_{ 0 }
 {
 }
 
-matrix_t::matrix_t( matrix_t const & other )
+matrix_t::matrix_t(matrix_t const& other)
 {
+    this->rows_ = other.rows_;
+    this->collumns_ = other.collumns_;
+    this->elements_ = new float*[this->rows_];
+    for (std::size_t i = 0; i < this->rows_; i++)
+    {
+        elements_[i] = new float[this->collumns_];
+        for (std::size_t j = 0; j < this->collumns_; j++)
+        {
+            this->elements_[i][j] = other.elements_[i][j];
+        }
+    }
 }
 
-matrix_t & matrix_t::operator =( matrix_t const & other )
+matrix_t& matrix_t::operator=(matrix_t const& other)
 {
-	return *this;
+    for (unsigned int i = 0; i < this->rows_; i++)
+    {
+        delete[] this->elements_[i];
+    }
+    delete[] this->elements_;
+    this->rows_ = other.rows_;
+    this->collumns_ = other.collumns_;
+    this->elements_ = new float*[rows_];
+    for (unsigned int i = 0; i < this->rows_; i++)
+    {
+        this->elements_[i] = new float[this->collumns_];
+        for (unsigned int j = 0; j < this->collumns_; j++)
+        {
+            this->elements_[i][j] = other.elements_[i][j];
+        }
+    }
+    return *this;
 }
 
 matrix_t::~matrix_t()
 {
+    for (unsigned int i = 0; i < this->rows_; i++)
+    {
+        delete[] this->elements_[i];
+    }
 }
 
 std::size_t matrix_t::rows() const
@@ -27,101 +61,243 @@ std::size_t matrix_t::collumns() const
     return collumns_;
 }
 
-matrix_t matrix_t::operator +( matrix_t const & other ) const
+matrix_t matrix_t::operator+(matrix_t const& other) const
 {
-	matrix_t result;
+    matrix_t result;
+    if (this->rows_ == other.rows_ && this->collumns_ == other.collumns_)
+    {
+        result.elements_ = new float*[this->rows_];
+        for (std::size_t i = 0; i < this->rows_; i++)
+        {
+            result.elements_[i] = new float[this->collumns_];
+        }
+        result.rows_ = this->rows_;
+        result.collumns_ = this->collumns_;
 
-	return result;
+        for (std::size_t i = 0; i < result.rows_; i++)
+        {
+            for (std::size_t j = 0; j < result.collumns_; j++)
+            {
+                result.elements_[i][j] = elements_[i][j] + other.elements_[i][j];
+            }
+        }
+    }
+    else
+    {
+        std::cout << std::endl << "fail";
+        exit(0);
+    }
+    return result;
 }
 
-matrix_t matrix_t::operator -( matrix_t const & other ) const
+matrix_t matrix_t::operator-(matrix_t const& other) const
 {
-	matrix_t result;
-
-	return result;
+    matrix_t result;
+    if (this->rows_ == other.rows_ && this->collumns_ == other.collumns_)
+    {
+        result.elements_ = new float*[this->rows_];
+        for (std::size_t i = 0; i < this->rows_; i++)
+        {
+            result.elements_[i] = new float[this->collumns_];
+        }
+        result.rows_ = this->rows_;
+        result.collumns_ = this->collumns_;
+        for (std::size_t i = 0; i < this->rows_; i++)
+        {
+            for (std::size_t j = 0; j < this->collumns_; j++)
+            {
+                result.elements_[i][j] = this->elements_[i][j] - other.elements_[i][j];
+            }
+        }
+    }
+    else
+    {
+        std::cout << std::endl << "fail";
+        exit(0);
+    }
+    return result;
 }
 
-matrix_t matrix_t::operator *( matrix_t const & other ) const
+matrix_t matrix_t::operator*(matrix_t const& other) const
 {
-	matrix_t result;
-
-	return result;
+    matrix_t result;
+    if (this->collumns_ == other.rows_)
+    {
+        result.rows_ = this->rows_;
+        result.collumns_ = other.collumns_;
+        result.elements_ = new float*[this->rows_];
+        for (std::size_t i = 0; i < this->rows_; ++i)
+        {
+            result.elements_[i] = new float[other.collumns_];
+        }
+        for (std::size_t i = 0; i < this->rows_; i++)
+        {
+            for (std::size_t j = 0; j < other.collumns_; j++)
+            {
+                int res = 0;
+                for (std::size_t k = 0; k < this->collumns_; k++)
+                {
+                    res += this->elements_[i][k] * other.elements_[k][j];
+                }
+                result.elements_[i][j] = res;
+            }
+        }
+    }
+    else
+    {
+        std::cout << std::endl << "fail";
+        exit(0);
+    }
+    return result;
 }
 
-matrix_t & matrix_t::operator -=( matrix_t const & other )
+matrix_t& matrix_t::operator-=(matrix_t const& other)
 {
-	return *this;
+    if (this->rows_ == other.rows_ && this->collumns_ == other.collumns_)
+    {
+        for (std::size_t i = 0; i < this->rows_; i++)
+        {
+            for (std::size_t j = 0; j < this->collumns_; j++)
+            {
+                this->elements_[i][j] -= other.elements_[i][j];
+            }
+        }
+    }
+    else
+    {
+        std::cout << std::endl << "fail";
+        exit(0);
+    }
+    return *this;
 }
 
-matrix_t & matrix_t::operator +=( matrix_t const & other )
+matrix_t& matrix_t::operator+=(matrix_t const& other)
 {
-	return *this;
+
+    if (this->rows_ == other.rows_ && this->collumns_ == other.collumns_)
+    {
+        for (std::size_t i = 0; i < this->rows_; i++)
+        {
+            for (std::size_t j = 0; j < this->collumns_; j++)
+            {
+                this->elements_[i][j] += other.elements_[i][j];
+            }
+        }
+    }
+    else
+    {
+        std::cout << std::endl << "fail";
+        exit(0);
+    }
+    return *this;
 }
 
-matrix_t & matrix_t::operator *=( matrix_t const & other )
+matrix_t& matrix_t::operator*=(matrix_t const& other)
 {
-	return *this;
+    if (this->collumns_ == other.rows_)
+    {
+        matrix_t result;
+        result.rows_ = this->rows_;
+        result.collumns_ = other.collumns_;
+        result.elements_ = new float*[this->rows_];
+        for (std::size_t i = 0; i < this->rows_; ++i)
+        {
+            result.elements_[i] = new float[other.collumns_];
+        }
+        for (std::size_t i = 0; i < this->rows_; i++)
+        {
+            for (std::size_t j = 0; j < other.collumns_; j++)
+            {
+                int res = 0;
+                for (std::size_t k = 0; k < this->collumns_; k++)
+                {
+                    res += this->elements_[i][k] * other.elements_[k][j];
+                }
+                result.elements_[i][j] = res;
+            }
+        }
+        *this = result;
+    }
+    else
+    {
+        std::cout << std::endl << "fail";
+        exit(0);
+    }
+    return *this;
 }
 
-std::istream & matrix_t::read( std::istream & stream )
+std::istream& matrix_t::read(std::istream& stream)
 {
     std::size_t rows;
     std::size_t collumns;
     char symbol;
-    
+
     bool success = true;
-    if( stream >> rows && stream >> symbol && symbol == ',' && stream >> collumns ) {
-        float ** elements = new float *[ rows ];
-        for( std::size_t i = 0; success && i < rows; ++i ) {
-            elements[ i ] = new float[ collumns ];
-            for( std::size_t j = 0; j < collumns; ++j ) {
-                if( !( stream >> elements[ i ][ j ] ) ) {
+    if (stream >> rows && stream >> symbol && symbol == ',' && stream >> collumns)
+    {
+        float** elements = new float*[rows];
+        for (std::size_t i = 0; success && i < rows; ++i)
+        {
+            elements[i] = new float[collumns];
+            for (std::size_t j = 0; j < collumns; ++j)
+            {
+                if (!(stream >> elements[i][j]))
+                {
                     success = false;
                     break;
                 }
             }
         }
-        
-        if( success ) {
-            for( std::size_t i = 0; i < rows_; ++i ) {
-                delete [] elements_[ i ];
+
+        if (success)
+        {
+            for (std::size_t i = 0; i < rows_; ++i)
+            {
+                delete[] elements_[i];
             }
-            delete [] elements_;
-            
+            delete[] elements_;
+
             rows_ = rows;
             collumns_ = collumns;
             elements_ = elements;
         }
-        else {
-            for( std::size_t i = 0; i < rows; ++i ) {
-                delete [] elements[ i ];
+        else
+        {
+            for (std::size_t i = 0; i < rows; ++i)
+            {
+                delete[] elements[i];
             }
-            delete [] elements;
+            delete[] elements;
         }
     }
-    else {
+    else
+    {
         success = false;
     }
-    
-    if( !success ) {
-        stream.setstate( std::ios_base::failbit );
+
+    if (!success)
+    {
+        stream.setstate(std::ios_base::failbit);
     }
-    
-	return stream;
+
+    return stream;
 }
 
-std::ostream & matrix_t::write( std::ostream & stream ) const
+std::ostream& matrix_t::write(std::ostream& stream) const
 {
     stream << rows_ << ", " << collumns_;
-    for( std::size_t i = 0; i < rows_; ++i ) {
+    for (std::size_t i = 0; i < rows_; ++i)
+    {
         stream << '\n';
-        for( std::size_t j = 0; j < collumns_; ++j ) {
-            stream << elements_[ i ][ j ];
-            if( j != rows_ - 1 ) {
+        for (std::size_t j = 0; j < collumns_; ++j)
+        {
+            stream << elements_[i][j];
+            if (j != rows_ - 1)
+            {
                 stream << ' ';
             }
         }
     }
-    
-	return stream;
+
+    return stream;
 }
